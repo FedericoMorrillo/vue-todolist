4b94137b09e0560846442d27981b63bc37bf43d2
FedericoMorrillo <federicomorrillo98@gmail.com>
2023-11-28 19:46:00 +0100

    feat: items e funzione rimuovi item

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 1) creiamo il codice di markup della to do list;
 2) creiamo un aray di oggetti con due informazioni;
 3) creiamo un contenitore dove all' interno ci saranno gli oggetti dell' array;
-4) se la seconda informazione dato che è boleano segna true all' oggetto diamo una classe css per sbarrare il testo;
+4) se la seconda informazione dato che è boleano segnato true all' oggetto diamo una classe css per sbarrare il testo;
 5) inseriamo di fianco all' oggetto un icona X dove se cliccata eliminerà l' oggetto dalla lista;
 6) utiliziamo un campo di imput per inserire un nuovo oggetto alla lista;
--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
 'use strict'
+
+const { createApp } = Vue
+
+  createApp({
+    data() {
+        
+        const items = [
+            {
+                text: "andare a fare la spesa",
+                done: false,
+            },
+            {
+                text: "studiare vue js",
+                done: false,
+            },
+            {
+                text: "andare in palestra",
+                done: false,
+            },
+        ]
+      return {
+        items,
+      }
+    },
+    methods:{
+        // funzione per impostare al click true o flase il done dell' oggetto
+        checks(item){
+            item.done = !item.done;
+            console.log(item)
+        },
+        //attraverso l' array rimuoviamo 1 oggetto selezionato partendo dall' indice scelto
+        removeToDo(index){
+            this.items.splice(index,1);
+        }
+    }
+  }).mount('#app')
